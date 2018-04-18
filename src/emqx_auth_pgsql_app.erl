478f7998b3bd3cf1759caa3abc61a5b7e0eb3adc
HeeeJianBo <heeejianbo@gmail.com>
2018-04-18 22:38:46 +0800

    Improve instance create logic

--- src/emqx_auth_pgsql_app.erl
+++ src/emqx_auth_pgsql_app.erl
@@ -41,16 +41,6 @@ start(_StartType, _StartArgs) ->
     %    ok = emqx_access_control:register_mod(acl, emqx_acl_pgsql, AclQuery)
     %end),
     %emqx_auth_pgsql_cfg:register(),
-    Schema = [[{key, host}, {default, ["127.0.0.1"]}, {required, false}, {descr, <<"Desc">>}],
-              [{key, port}, {default, [5432]}, {required, false}, {descr, <<"Desc">>}],
-              [{key, username}, {default, ["root"]}, {required, false}, {descr, <<"Desc">>}],
-              [{key, database}, {default, ["mqtt"]}, {required, false}, {descr, <<"Desc">>}],
-              [{key, encoding}, {default, ["utf8"]}, {required, false}, {descr, <<"Desc">>}],
-              [{key, ssl}, {default, [false]}, {required, false}, {descr, <<"Desc">>}],
-              [{key, auth_query}, {default, ["select password from mqtt_user where username = '%u' limit 1"]}, {required, false}, {descr, <<"Desc">>}],
-              [{key, password_hash}, {default, [sha256, plain, md5, sha, bcrypt]}, {required, false}, {descr, <<"Desc">>}],
-              [{key, super_query}, {default, ["select is_superuser from mqtt_user where username = '%u' limit 1"]}, {required, false}, {descr, <<"Desc">>}],
-              [{key, acl_query}, {default, ["select allow, ipaddr, username, clientid, access, topic from mqtt_acl where ipaddr = '%a' or username = '%u' or username = '$all' or clientid = '%c'"]}, {required, false}, {descr, <<"Desc">>}]],
     ok = emqx_services:register(emqx_auth_pgsql_ins, ?APP, auth, []),
 
     {ok, Sup}.
@@ -60,10 +50,4 @@ stop(_State) ->
     %emqx_access_control:unregister_mod(auth, emqx_auth_pgsql),
     %emqx_auth_pgsql_cfg:unregister().
     emqx_services:unregister(emqx_auth_pgsql_ins).
-
-if_enabled(Par, Fun) ->
-    case application:get_env(?APP, Par) of
-        {ok, Query} -> Fun(parse_query(Query));
-        undefined   -> ok
-    end.
 
--- src/emqx_auth_pgsql_ins.erl
+++ src/emqx_auth_pgsql_ins.erl
@@ -30,33 +30,46 @@ init(_Env) -> ok.
 
 create(Conf, _Env) ->
     lager:debug("create instance ~p", [Conf]),
-    %% TODO: register Hooks, establish connection
-    AuthQuery = parse_query(value(auth_query, Conf)),
-    SuperQuery = parse_query(value(super_query, Conf)),
-    HashType = value(password_hash, Conf),
-    AuthEnv = {AuthQuery, SuperQuery, HashType},
-    %% TODO: Can't register mutil instances
-    ok = emqx_access_control:register_mod(auth, emqx_auth_pgsql, AuthEnv),
 
-    AclQuery = parse_query(value(acl_query, Conf)),
-
-    ok = emqx_access_control:register_mod(acl, emqx_acl_pgsql, AclQuery),
-    emqx_auth_pgsql_cfg:register(),
-
-    %% Establish connections
-    %% XXX: Need huang to main superviser
     PoolSpec = ecpool:pool_spec(?APP, ?APP, emqx_auth_pgsql_cli, value(server, Conf)),
-    supervisor:start_child(emqx_auth_pgsql_sup, PoolSpec),
-    ok.
+    case supervisor:start_child(emqx_auth_pgsql_sup, PoolSpec) of
+        {ok, _Pid} ->
+            %% Register AUTH/ACL, Hook etc.
+            if_enabled(auth_query, Conf, fun(AuthQuery) ->
+                SuperQuery = parse_query(value(super_query, Conf)),
+                HashType = value(password_hash, Conf),
+                AuthEnv = {AuthQuery, SuperQuery, HashType},
+                %% TODO: Can't register mutil instances
+                ok = emqx_access_control:register_mod(auth, emqx_auth_pgsql, AuthEnv)
+            end),
+            if_enabled(acl_query, Conf, fun(AclQuery) ->
+                ok = emqx_access_control:register_mod(acl, emqx_acl_pgsql, AclQuery)
+            end),
+            emqx_auth_pgsql_cfg:register(), ok;
+        {error, Reason} ->
+            lager:error("start connection pool error ~p", [Reason]),
+            {error, Reason}
+    end.
 
 destroy() ->
     emqx_access_control:unregister_mod(acl, emqx_acl_pgsql),
     emqx_access_control:unregister_mod(auth, emqx_auth_pgsql),
     emqx_auth_pgsql_cfg:unregister(),
     supervisor:terminate_child(emqx_auth_pgsql_sup, ?APP),
+    supervisor:delete_child(emqx_auth_pgsql_sup, ?APP),
     ok.
 
 description() -> "Auth/ACL for PostgreSQL".
+
+%%--------------------------------------------------------------------
+%% Interval Functions
+%%--------------------------------------------------------------------
+
+if_enabled(Par, Conf, Fun) ->
+    case value(Par, Conf) of
+        undefined   -> ok;
+        Query -> Fun(parse_query(Query))
+    end.
 
 value(Key, List) -> proplists:get_value(Key, List).
 
