2956ad75ae7bfadb742d38b54e2c0df9137bced5
HeeeJianBo <heeejianbo@gmail.com>
2018-04-27 11:46:41 +0800

    Delete emqx_auth_pgsql_ins

--- src/emqx_auth_pgsql_app.erl
+++ src/emqx_auth_pgsql_app.erl
@@ -21,9 +21,15 @@
 -include("emqx_auth_pgsql.hrl").
 
 -import(emqx_auth_pgsql_cli, [parse_query/1]).
+-import(proplists, [get_value/2]).
 
 %% Application callbacks
 -export([start/2, stop/1]).
+
+-behaviour(emqx_services).
+
+%% emqx_services callbacks
+-export([create/2, destroy/0, description/0]).
 
 %%--------------------------------------------------------------------
 %% Application callbacks
@@ -31,23 +37,55 @@
 
 start(_StartType, _StartArgs) ->
     {ok, Sup} = emqx_auth_pgsql_sup:start_link(),
-    %if_enabled(auth_query, fun(AuthQuery) ->
-    %    SuperQuery = parse_query(application:get_env(?APP, super_query, undefined)),
-    %    {ok, HashType}  = application:get_env(?APP, password_hash),
-    %    AuthEnv = {AuthQuery, SuperQuery, HashType},
-    %    ok = emqx_access_control:register_mod(auth, emqx_auth_pgsql, AuthEnv)
-    %end),
-    %if_enabled(acl_query, fun(AclQuery) ->
-    %    ok = emqx_access_control:register_mod(acl, emqx_acl_pgsql, AclQuery)
-    %end),
-    %emqx_auth_pgsql_cfg:register(),
-    ok = emqx_services:register(emqx_auth_pgsql_ins, ?APP, auth, []),
+    ok = emqx_services:register(?APP, auth, []),
 
     {ok, Sup}.
 
 stop(_State) ->
-    %emqx_access_control:unregister_mod(acl, emqx_acl_pgsql),
-    %emqx_access_control:unregister_mod(auth, emqx_auth_pgsql),
-    %emqx_auth_pgsql_cfg:unregister().
-    emqx_services:unregister(emqx_auth_pgsql_ins).
+    emqx_services:unregister(?APP).
+
+%%--------------------------------------------------------------------
+%% Service callback
+%%--------------------------------------------------------------------
+
+create(Conf, _Env) ->
+    PoolSpec = ecpool:pool_spec(?APP, ?APP, emqx_auth_pgsql_cli, get_value(server, Conf)),
+    case supervisor:start_child(emqx_auth_pgsql_sup, PoolSpec) of
+        {ok, _Pid} ->
+            %% Register AUTH/ACL, Hook etc.
+            if_enabled(auth_query, Conf, fun(AuthQuery) ->
+                SuperQuery = parse_query(get_value(super_query, Conf)),
+                HashType = get_value(password_hash, Conf),
+                AuthEnv = {AuthQuery, SuperQuery, HashType},
+                %% TODO: Can't register mutil instances
+                ok = emqx_access_control:register_mod(auth, emqx_auth_pgsql, AuthEnv)
+            end),
+            if_enabled(acl_query, Conf, fun(AclQuery) ->
+                ok = emqx_access_control:register_mod(acl, emqx_acl_pgsql, AclQuery)
+            end),
+            emqx_auth_pgsql_cfg:register(), ok;
+        {error, Reason} ->
+            lager:error("start connection pool error ~p", [Reason]),
+            {error, Reason}
+    end.
+
+destroy() ->
+    emqx_access_control:unregister_mod(acl, emqx_acl_pgsql),
+    emqx_access_control:unregister_mod(auth, emqx_auth_pgsql),
+    emqx_auth_pgsql_cfg:unregister(),
+    supervisor:terminate_child(emqx_auth_pgsql_sup, ?APP),
+    supervisor:delete_child(emqx_auth_pgsql_sup, ?APP),
+    ok.
+
+description() -> "Auth/ACL for PostgreSQL".
+
+%%--------------------------------------------------------------------
+%% Interval Functions
+%%--------------------------------------------------------------------
+
+if_enabled(Par, Conf, Fun) ->
+    case get_value(Par, Conf) of
+        undefined   -> ok;
+        Query -> Fun(parse_query(Query))
+    end.
 
