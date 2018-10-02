078474b65fb31f04903d803c0b132da0972600e7
turtled <turtled@emqtt.io>
2018-10-03 07:45:56 +0800

    Rename table name(devices -> clients)

--- src/emqx_auth_pgsql.erl
+++ src/emqx_auth_pgsql.erl
@@ -29,7 +29,7 @@ check(lwm2m, _Password, Client = #mqtt_client{client_id = ClientId}, _Env) ->
                {lwm2m_dn_dm_topic, {<<"inbox">>, 0}},
                {lwm2m_model, psm}],
     % {ok, Client#mqtt_client{headers = Headers}};
-    AuthSql = "select \"tenantID\", \"productID\", \"deviceID\", \"autoSub\", blocked from devices where \"deviceID\" = $1 limit 1",
+    AuthSql = "select \"tenantID\", \"productID\", \"deviceID\", \"autoSub\", blocked from clients where \"deviceID\" = $1 limit 1",
     case emqx_auth_pgsql_cli:equery(AuthSql, [ClientId]) of
         {ok, _, [{_TenantId, _ProductId, _DeviceId, _AutoSub, 1}]} ->
             lager:error("ClientId '~s' login failed for black list", [ClientId]),
@@ -52,12 +52,15 @@ check(lwm2m, _Password, Client = #mqtt_client{client_id = ClientId}, _Env) ->
 check(mqtt, Password, Client = #mqtt_client{headers = Headers, username = Username} , {{AuthSql, AuthParams}, HashType}) ->
     lager:debug("Sql:~p, params:~p", [AuthSql, AuthParams]),
     case emqx_auth_pgsql_cli:equery(AuthSql, AuthParams, Client) of
-        {ok, _, [{_TenantId, _ProductId, _GroupId, _DeviceId, _DeviceUsername, _Token, 1, _}]} ->
+        {ok, _, [{_TenantId, _ProductId, _GroupId, _DeviceId, _DeviceUsername, _Token, 1, _, _}]} ->
             lager:error("Username '~s' login failed for black list", [Username]),
             {stop, Client};
-        {ok, _, [{TenantId, ProductId, GroupId, DeviceId, Username, Token, _Status, 1}]} ->
-            Headers1 = mqtt_headers(TenantId, ProductId, GroupId, DeviceId, Headers),
-            Mountpoint = mountpoint(<<"mqtt">>, TenantId, ProductId, DeviceId),
+        {ok, _, [{TenantId, ProductId, GroupId, DeviceId, Username, Token, _Status, 1, Type}]} ->
+            Headers1 = mqtt_headers(TenantId, ProductId, GroupId, DeviceId, Type, Headers),
+            Mountpoint = case Type of
+                2 -> mountpoint(<<"mqtt">>, TenantId, DeviceId);
+                _ -> mountpoint(<<"mqtt">>, TenantId, ProductId, DeviceId)
+            end,
             case check_pass({Token}, Password, HashType) of
                 ok ->
                     ClientId2 = <<TenantId/binary, ":", ProductId/binary, ":", DeviceId/binary>>,
@@ -66,12 +69,15 @@ check(mqtt, Password, Client = #mqtt_client{headers = Headers, username = Userna
                     lager:error("Username '~s' login failed for ~p", [Username, Error]),
                     {stop, Client#mqtt_client{headers = Headers1}}
             end;
-        {ok, _, [{TenantId, ProductId, GroupId, DeviceId, _, Token, _Status, 1}]} ->
+        {ok, _, [{TenantId, ProductId, GroupId, DeviceId, _, Token, _Status, 1, _}]} ->
             Headers2 = mqtt_headers(TenantId, ProductId, GroupId, DeviceId, Headers),
             {stop, Client#mqtt_client{headers = Headers2}};
-        {ok, _, [{TenantId, ProductId, GroupId, DeviceId, _DeviceUsername, _Token, _Status, 2}]} ->
-            Headers3 = mqtt_headers(TenantId, ProductId, GroupId, DeviceId, Headers),
-            Mountpoint = mountpoint(<<"mqtt">>, TenantId, ProductId, DeviceId),
+        {ok, _, [{TenantId, ProductId, GroupId, DeviceId, _DeviceUsername, _Token, _Status, 2, Type}]} ->
+            Headers3 = mqtt_headers(TenantId, ProductId, GroupId, DeviceId, Type, Headers),
+            Mountpoint = case Type of
+                2 -> mountpoint(<<"mqtt">>, TenantId, DeviceId);
+                _ -> mountpoint(<<"mqtt">>, TenantId, ProductId, DeviceId)
+            end,
             ClientId3 = <<TenantId/binary, ":", ProductId/binary, ":", DeviceId/binary>>,
             Sql = "select id from cert_auth where \"clientID\" = $1 and \"CN\" = $2 and enable = 1 limit 1",
             CN = proplists:get_value(cn, Headers),
@@ -111,11 +117,17 @@ hash(Type, Password) ->
     emqx_auth_mod:passwd_hash(Type, Password).
 
 mqtt_headers(TenantId, ProductId, GroupId, DeviceId, Headers) ->
+    mqtt_headers(TenantId, ProductId, GroupId, DeviceId, 1, Headers).
+mqtt_headers(TenantId, ProductId, GroupId, DeviceId, Type, Headers) ->
     [{tenant_id, TenantId},
      {product_id, ProductId},
      {group_id, GroupId},
      {device_id, DeviceId},
-     {is_superuser, false} | Headers].
+     {is_superuser, false},
+     {type, Type} | Headers].
+
+mountpoint(Protocol, TenantId, DeviceId) ->
+    emqx_topic:encode(<<>>, [Protocol, TenantId, DeviceId]).
 
 mountpoint(Protocol, TenantId, ProductId, DeviceId) ->
     emqx_topic:encode(<<>>, [Protocol, TenantId, ProductId, DeviceId]).
